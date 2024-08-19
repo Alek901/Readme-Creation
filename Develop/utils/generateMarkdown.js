16af30ec55e2672c982b098ab1add7aeb58d71b5
Alek Gonzalez <Magicguys1@icloud.com>
2024-08-19 13:12:16 -0600

    Revert "changed License section and added more to the read comments"
    This reverts commit 7ac0de26434bd6c399d4ce34f8321061859e384a.

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -35,31 +35,20 @@ console.log(noLink); // if none provided it outputs: ''
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  const licenseBadges = {
-    'MIT': 'https://img.shields.io/badge/License-MIT-yellow.svg',
-    'Apache-2.0': 'https://img.shields.io/badge/License-Apache%202.0-blue.svg',
-    'GPL-3.0': 'https://img.shields.io/badge/License-GPLv3-blue.svg',
-    'BSD-3-Clause': 'https://img.shields.io/badge/License-BSD%203--Clause-orange.svg',
-    'MPL-2.0': 'https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg',
-  };
-
-  const licenseLinks = {
-    'MIT': 'https://opensource.org/licenses/MIT',
-    'Apache-2.0': 'https://opensource.org/licenses/Apache-2.0',
-    'GPL-3.0': 'https://opensource.org/licenses/GPL-3.0',
-    'BSD-3-Clause': 'https://opensource.org/licenses/BSD-3-Clause',
-    'MPL-2.0': 'https://opensource.org/licenses/MPL-2.0',
-  };
-
-  if (!license || !licenseBadges[license] || !licenseLinks[license]) {
+  if (!license) {
     return '';
   }
 
-  const badgeUrl = licenseBadges[license];
-  const linkUrl = licenseLinks[license];
-  const badgeText = license;
+  const badgeUrl = renderLicenseBadge(license);
+  const licenseLink = renderLicenseLink(license);
 
-  return `## License\n\n![License Badge](${badgeUrl} "${badgeText} License")\n\nThis project is licensed under the [${badgeText} License](${linkUrl}).`;
+  return `
+## License
+
+This project is licensed under the [${license} License](${licenseLink}).
+
+![License Badge](${badgeUrl})
+    `.trim();
 }
 
 
@@ -69,13 +58,6 @@ function generateMarkdown(data) {
   const data = {
     title: 'ReadmeMagicCreation',
     description: 'come back later and fill.',
-    tableofcontents: '',
-    installation: '',
-    usage: '',
-    license: '',
-    contributing: '',
-    tests: '',
-    questions: '',
   }
 
 
@@ -93,8 +75,7 @@ function generateMarkdown(data) {
   ## Usage
   ${data.usage}
 
-  ## License
-  ${data.license}
+ 
 
   ## Contributing
   ${data.contributing}
