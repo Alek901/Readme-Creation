1158329a5c56e310f3519ddcc762841ace4eca04
Alek Gonzalez <Magicguys1@icloud.com>
2024-08-19 14:29:58 -0600

    changed all
    messed up. started over. changed all so there would be multiple licenses and links. Also be able to return as an empty string if no license is selected.

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,54 +1,55 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  let badgeUrl = '';
+    if (!license) {
+      return ''; // Return an empty string if there is no license
+    }
 
-  switch (license) {
-    case 'ISC':
-      badgeUrl = 'https://img.shields.io/badge/License-ISC-blue.svg';
-      break;
+    // License badges map
+    const licenseBadges = {
+      'MIT': '![MIT License](https://img.shields.io/badge/License-MIT-yellow.svg)',
+      'GPLv3': '![GPLv3 License](https://img.shields.io/badge/License-GPLv3-blue.svg)',
+      'Apache 2.0': '![Apache License](https://img.shields.io/badge/License-Apache%202.0-green.svg)',
+      'BSD 3-Clause': '![BSD License](https://img.shields.io/badge/License-BSD%203--Clause-orange.svg)',
+      // Add more licenses as needed
+    };
+
+    return licenseBadges[license] || ''; // Return the corresponding badge or an empty string if the license is not found
   }
-
-  return badgeUrl;
-}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
+  if (!license) {
+    return ''; // Return an empty string if there is no license
+  }
+
+  // License links map
   const licenseLinks = {
     'MIT': 'https://opensource.org/licenses/MIT',
-    'Apache-2.0': 'https://opensource.org/licenses/Apache-2.0',
-    'GPL-3.0': 'https://opensource.org/licenses/GPL-3.0',
-    'BSD-3-Clause': 'https://opensource.org/licenses/BSD-3-Clause',
-    'MPL-2.0': 'https://opensource.org/licenses/MPL-2.0',
+    'GPLv3': 'https://www.gnu.org/licenses/gpl-3.0',
+    'Apache 2.0': 'https://opensource.org/licenses/Apache-2.0',
+    'BSD 3-Clause': 'https://opensource.org/licenses/BSD-3-Clause',
+    // Add more licenses as needed
   };
 
-  return license ? `[${license} License](${licenseLinks[license] || ''})` : '';
+  return licenseLinks[license] || ''; // Return the corresponding link or an empty string if the license is not found
 }
-
-
-const noLicense = '';
-const noLink = renderLicenseLink(noLicense);
-console.log(noLink); // if none provided it outputs: ''
-
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (!license) {
-    return '';
+    return ''; // Return an empty string if there is no license
   }
 
-  const badgeUrl = renderLicenseBadge(license);
   const licenseLink = renderLicenseLink(license);
 
   return `
 ## License
 
-This project is licensed under the [${license} License](${licenseLink}).
-
-![License Badge](${badgeUrl})
-    `.trim();
+This project is licensed under the ${license} license. For more information, see [here](${licenseLink}).
+    `;
 }
 
 
